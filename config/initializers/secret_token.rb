847d9afe107c0b807869f9d2e6534b6426f19e5e
Michael Bleigh <michael@intridea.com>
2011-04-21 22:55:15 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clublet::Application.config.secret_token = 'b0bd7ebd6703cb4075f26ad228a2f1aa99d9fdba7d74f64150b7e60aae9546b1b28914198894c1fcb53a4597f28fa4dab49e637df819d199a2cc67911a990ba0'
