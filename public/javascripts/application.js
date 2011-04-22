006235eb9a5d5e1d2815fcedfeb201d257eae278
Bradley Grzesiak <brad@bendyworks.com>
2011-04-22 01:19:43 -0500

    Add coffeescript and barista

---
+++ public/javascripts/application.js
@@ -0,0 +1,7 @@
+/* DO NOT MODIFY. This file was compiled Fri, 22 Apr 2011 06:18:33 GMT from
+ * /Users/brad/dev/clublet/app/coffeescripts/application.coffee
+ */
+
+(function() {
+  $(function() {});
+}).call(this);
