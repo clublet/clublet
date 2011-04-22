b52080e04a8e6e54ad39318d6fe455f22aa57706
Michael Bleigh <michael@intridea.com>
2011-04-22 00:50:41 -0500

    Adds a logo. Hooray.

---
+++ app/controllers/clubs_controller.rb
@@ -0,0 +1,4 @@
+class ClubsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Clublet::Application.routes.draw do
+  root :to => 'clubs#index'
   match '/auth/:provider/callback', :to => 'sessions#callback'
-
 end
